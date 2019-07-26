be174c5221cb7c034b4c20b613dc3466fb52034a
farazahmadkhan15 <48973967+farazahmadkhan15@users.noreply.github.com>
2019-07-26 07:06:50 -0700

    comm

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -330,17 +330,17 @@
     |=================
     */
       
-      $("#contactForm").validator().on("submit", function (event) {
-          if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            formError();
-            submitMSG(false, "Did you fill in the form properly?");
-          } else {
-            // everything looks good!
-            event.preventDefault();
+      // $("#contactForm").validator().on("submit", function (event) {
+      //     if (event.isDefaultPrevented()) {
+      //       // handle the invalid form...
+      //       formError();
+      //       submitMSG(false, "Did you fill in the form properly?");
+      //     } else {
+      //       // everything looks good!
+      //       event.preventDefault();
        
-          }
-       });
+      //     }
+      //  });
     
        //  function submitForm(){
        //    var name = $("#name").val();
@@ -361,23 +361,23 @@
        //          }
        //      });
        //  }
-         function formSuccess(){
-             $("#contactForm")[0].reset();
-             submitMSG(true, "Message Sent!")
-         }
-    	  function formError(){   
-    	    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-    	        $(this).removeClass();
-    	    });
-    	  }
-        function submitMSG(valid, msg){
-          if(valid){
-            var msgClasses = "h3 text-center fadeInUp animated text-success";
-          } else {
-            var msgClasses = "h3 text-center shake animated text-danger";
-          }
-          $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-        }
+       //   function formSuccess(){
+       //       $("#contactForm")[0].reset();
+       //       submitMSG(true, "Message Sent!")
+       //   }
+    	  // function formError(){   
+    	  //   $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    	  //       $(this).removeClass();
+    	  //   });
+    	  // }
+       //  function submitMSG(valid, msg){
+       //    if(valid){
+       //      var msgClasses = "h3 text-center fadeInUp animated text-success";
+       //    } else {
+       //      var msgClasses = "h3 text-center shake animated text-danger";
+       //    }
+       //    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+       //  }
     
 
     
